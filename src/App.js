fa8d350fa91954dc74c9a34439ca6f0ed467e298
Esther12 <yating_su@outlook.com>
2022-01-04 01:01:28 -0500

    add the changes for the nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import './asset/App.css';
-import 'material-icons/iconfont/material-icons.css';
-import BookList from './components/bookList';
-import SearchBox from './components/searchBox';
-import { useSelector } from 'react-redux';
+import "./asset/App.css";
+import "material-icons/iconfont/material-icons.css";
+import BookList from "./components/bookList";
+import SearchBox from "./components/searchBox";
+import { useSelector } from "react-redux";
 import Loading from "./asset/loading.svg";
 
 function App() {
-  const books = useSelector(state => state.searchReducer)
+  const books = useSelector(state => state.searchReducer);
   return (
-    <div className='container'>
-      <SearchBox/>
-      {books.current_books.length > 1 && !books.loading ? <BookList/> : books.loading ? <img className="svg-loading" src={Loading} alt={"loading"} /> : null}
+    <div>
+      <nav id="navbar">
+        <div className="nav-wrapper">
+          <div className="logo">
+            <h3>Quick Search</h3>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <SearchBox />
+        {books.current_books.length > 1 && !books.loading ? <BookList /> : books.loading ? <img className="svg-loading" src={Loading} alt={"loading"} /> : null}
+      </div>
     </div>
   );
 }
 export default App;
-
-
-
